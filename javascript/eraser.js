e4b336b64602e83e6859e02a1b3380822ca0ca41
MrLawli3t <sivertgabriel@gmail.com>
2025-09-14 01:21:01 +0200

    cleaned up code structure

--- javascript/eraser.js
+++ javascript/eraser.js
@@ -1,15 +1,8 @@
-onUiLoaded(() => {
-    const inpaintTab = gradioApp().querySelector("#img2img_inpaint_tab");
-    const maskCanvas = inpaintTab.querySelector("canvas");
-    
-    if (!maskCanvas) return;
-
-    const toolbar = inpaintTab.querySelector(".forge-toolbar-box-a");
+function createEraserButton() {
     const eraserBtn = document.createElement('button');
     eraserBtn.innerText = '🧼';
     eraserBtn.title = "Eraser";
     eraserBtn.class = "forge-btn";
-    toolbar.appendChild(eraserBtn);
 
     let erasing = false;
 
@@ -19,8 +12,10 @@ onUiLoaded(() => {
     eraserBtn.onmouseout = () => { 
         eraserBtn.style.opacity = 1;
     }
-    eraserBtn.onclick = () => { 
-        const ctx = maskCanvas.getContext('2d');
+    eraserBtn.onclick = (e) => {
+        const parent = e.target.closest(".tabitem");
+        const canvas = parent.querySelector("canvas");
+        const ctx = canvas.getContext('2d');
         erasing = !erasing;
 
         if (erasing) {
@@ -31,4 +26,15 @@ onUiLoaded(() => {
 
         ctx.globalCompositeOperation = erasing ? "destination-out" : "source-over";
     }
+
+    return eraserBtn;
+}
+
+onUiLoaded(() => {
+    const inpaintTab = gradioApp().querySelector("#img2img_inpaint_tab");
+
+    const inpaintToolbar = inpaintTab.querySelector(".forge-toolbar-box-a");
+
+    inpaintToolbar.appendChild(createEraserButton());
+
 });
