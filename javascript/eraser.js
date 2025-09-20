55415abed717fdb574fa037228c8b0ae501a7a06
MrLawli3t <sivertgabriel@gmail.com>
2025-09-20 13:29:45 +0200

    Added support for reForge

--- javascript/eraser.js
+++ javascript/eraser.js
@@ -1,4 +1,4 @@
-function createEraserButton() {
+function createEraserButton(isReforge = false) {
     const eraserBtn = document.createElement('button');
     eraserBtn.innerText = '🧼';
     eraserBtn.title = "Eraser";
@@ -14,7 +14,14 @@ function createEraserButton() {
     }
     eraserBtn.onclick = (e) => {
         const parent = e.target.closest(".tabitem");
-        const canvas = parent.querySelector("canvas");
+        let canvas;
+
+        if (isReforge) {
+            canvas = parent.querySelector("canvas[key='mask']");
+        } else {
+            canvas = parent.querySelector("canvas");
+        }
+        
         const ctx = canvas.getContext('2d');
         erasing = !erasing;
 
@@ -33,8 +40,53 @@ function createEraserButton() {
 onUiLoaded(() => {
     const inpaintTab = gradioApp().querySelector("#img2img_inpaint_tab");
 
+    // Forge and Forge Classic
     const inpaintToolbar = inpaintTab.querySelector(".forge-toolbar-box-a");
 
-    inpaintToolbar.appendChild(createEraserButton());
+    // Reforge
+    if (inpaintToolbar == null) {
+        let isEraserAdded = false;
+        const imageContainer = inpaintTab.querySelector(".image-container");
+        let eraserBtn;
+
+        if (imageContainer == null) {
+            console.log("Image container not found");
+            return;
+        }
+
+        const observer = new MutationObserver((mutationsList) => {
+            for (const mutation of mutationsList) {
+                if (mutation.type === "childList") {
+                    if (mutation.addedNodes.length === 0 && mutation.removedNodes.length === 0) {
+                    return;
+                }
+
+                if (imageContainer.children[0].children[3] == null) {
+                    return;
+                }
+
+                if (!isEraserAdded) {
+                    eraserBtn = createEraserButton(true);
+                    imageContainer.children[0].children[3].appendChild(eraserBtn);
+                    isEraserAdded = true;
+
+                    // Attach remove button listener right away
+                    const removeButton = imageContainer.querySelector("button[title='Remove Image']");
+                    if (removeButton && !removeButton.dataset.eraserListener) {
+                        removeButton.addEventListener("click", () => {
+                            eraserBtn.remove();
+                            isEraserAdded = false;
+                        });
+                        removeButton.dataset.eraserListener = "true"; // avoid duplicates
+                    }
+                }
+                }
+            }
+        });
+
+        observer.observe(imageContainer.children[0], { childList: true, subtree: false });
+    } else {
+        inpaintToolbar.appendChild(createEraserButton());
+    }
 
 });
